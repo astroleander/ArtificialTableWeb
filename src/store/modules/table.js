5cf4409363960aca4e434b5553e4b8714b487a04
Astroleander <astroleander@gmail.com>
2018-12-07 21:23:26 +0800

    Update input modules

--- src/store/modules/table.js
+++ src/store/modules/table.js
@@ -1,14 +1,29 @@
 const table = {
   state: {
-    importTable: []
+    importTable: [],
+    settingsTable: {},
+    previewTable: {}
   },
   mutations: {
     SAVE_IMPORT_TABLE: (state, { table }) => {
       state.importTable = table
     },
+    SAVE_SETTINGS_TABLE: (state, { table }) => {
+      state.settingsTable = table
+    },
+    SAVE_PREVIEW_TABLE: (state, { table }) => {
+      state.previewTable = table
+    },
     DELETE_IMPORT_TABLE: (state) => {
       state.importTable = []
+    },
+    DELETE_SETTINGS_TABLE: (state) => {
+      state.settingsTable = []
+    },
+    DELETE_PREVIEW_TABLE: (state) => {
+      state.previewTable = []
     }
+
   },
   actions: {
     saveImportTable({ commit, state }, { table }) {
@@ -17,14 +32,33 @@ const table = {
         resolve()
       })
     },
-    getImportTable({ commit, state }) {
+    saveSettingsTable({ commit, state }, { table }) {
       return new Promise((resolve, reject) => {
-        resolve(state.importTable)
+        commit('SAVE_SETTINGS_TABLE', { table })
+        resolve()
+      })
+    },
+    savePreviewTable({ commit, state }, { table }) {
+      return new Promise((resolve, reject) => {
+        commit('SAVE_PREVIEW_TABLE', { table })
+        resolve()
       })
     },
     deleteImportTable({ commit, state }) {
       return new Promise((resolve, reject) => {
         commit('DELETE_IMPORT_TABLE')
+        resolve()
+      })
+    },
+    deleteSettingsTable({ commit, state }) {
+      return new Promise((resolve, reject) => {
+        commit('DELETE_SETTINGS_TABLE')
+        resolve()
+      })
+    },
+    deletePreviewTable({ commit, state }) {
+      return new Promise((resolve, reject) => {
+        commit('DELETE_PREVIEW_TABLE')
         resolve()
       })
     }
