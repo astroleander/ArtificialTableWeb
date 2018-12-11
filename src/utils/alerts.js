2432b7bd73105557412956307b3a2572e146e063
Astroleander <astroleander@gmail.com>
2018-12-11 19:47:33 +0800

    Extract alert from components

---
+++ src/utils/alerts.js
@@ -0,0 +1,38 @@
+export const CLIP_BOARD_ALERT = {
+  id: '0',
+  title: '请使用 「Ctrl+V」 进行粘贴',
+  description: '出于安全因素的考虑，现代浏览器不允许网页自动从您的剪切板中获取数据。',
+}
+
+export const REQUIRE_STUDENT_COLUMN = {
+  id: '1',
+  title: '您可能缺少学号列',
+  description: ''
+}
+
+export const REQUIRE_STUDENT_COLUMN_LEFT = {
+  id: '2',
+  title: '您的最左侧的列不是最长的列, 您的最左列不是学号吗?',
+  description: '您的拥有最大行数的列未放置在最左侧, 这不会导致系统导入的问题, 但是您最好确认所有学生的学号已经导入'
+}
+
+export const DUPLICATE_SID = {
+  id: '114154',
+  title: '重复的学号列',
+  description: '系统检测到多个学号列, 请删除多余的学号列后再进行下一步。',
+  type: 'error'
+}
+
+export const REQUIRED_SID = {
+  id: 'AEDVHF',
+  title: '缺少学号列',
+  description: '系统没有检测到学号列, 请添加学号列后再进行下一步。',
+  type: 'error'
+}
+
+export const REQUIRED_TITLE = {
+  id: 'AFIAFHJAIOD',
+  title: '您需要至少设置一个导入的项。',
+  type: 'error'
+}
+
