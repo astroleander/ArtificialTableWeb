5435c950b2702f53e6d0aeaeb418c54de84808f9
Astroleander <astroleander@gmail.com>
2018-12-13 18:19:22 +0800

    Qian Li's work

---
+++ src/components/Weight/LessonCard.vue
@@ -0,0 +1,392 @@
+<template>
+  <el-card class="box-card" shadow="hover"  v-if="dataSet">
+    <div slot="header" >
+      <div class="row-frame1">
+        <div class="groupName">
+          {{groupName}}
+        </div>
+        <div class="error-box">
+          <el-alert v-show="isError"
+                    :title="errorMsg" type="error" :closable="false" show-icon>
+          </el-alert>
+        </div>
+        <div class="button-group">
+          <el-button @click="dealLeft">一键分配</el-button>
+          <el-button @click="addLessonItem">添加大项</el-button>
+          <el-button @click="dealCancel">还原</el-button>
+          <el-button type="primary" @click="openDialog" :disabled="modifyDisabled">修改</el-button>
+        </div>
+      </div>
+    </div>
+    <div class="row-frame2">
+      <div  class="titles">
+        <div  class="title-box"
+              v-for="(title,index) in currentDataSet"
+              :key="title.id"
+              :idx="index">
+          <div class="tag-box">
+            <el-tag  >
+              {{title.name}}
+            </el-tag>
+          </div>
+          <el-input class="inputBox"
+                    type="text"
+                    size="small"
+                    v-model="title.weight"
+                    :maxlength="3"
+                    :disabled="titleDisabled[index]"
+                    @change="handleInput(title.weight,index)"/>
+          <div  class="slider-box">
+            <vue-slide-bar v-model="title.weight"
+                           :lineHeight="10"
+                           :min="0"
+                           :max="100"
+                           :draggable="!titleDisabled[index]"
+                           :is-disabled="titleDisabled[index]"
+                           @input="handleInput(title.weight,index)"/>
+          </div>
+          <div class="lock-box" :class="{'active':titleDisabled[index]}" @click="changeLock(index)" >锁定</div>
+        </div>
+      </div>
+      <at-pie class="pie-box" :dataSet="currentDataSet" :groupId="groupId"></at-pie>
+      <el-dialog title="添加大项" :visible.sync="dialogFormVisible">
+        <el-form :model="NewTitleGroup" status-icon :rules="rules" ref="ruleForm">
+          <el-form-item label="名称" prop="name" :label-width="formLabelWidth">
+            <el-input v-model="NewTitleGroup.name"    placeholder="请输入名称" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="权重" prop="weight" :label-width="formLabelWidth">
+            <el-input  v-model.number="NewTitleGroup.weight"   placeholder="请输入权重值（0-100之间）" ></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
+        </div>
+      </el-dialog>
+    </div>
+  </el-card>
+</template>
+
+<script>
+  import VueSlideBar from 'vue-slide-bar'
+  import AtPie from '@/components/Weight/Pie'
+  export default {
+    name: 'LessonCard',
+    data() {
+      var checkWeight = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('权重不能为空'))
+        }
+        if (!Number.isInteger(value)) {
+          callback(new Error('请输入数字值'))
+        } else {
+          if (value > 100 || value < 0) {
+            callback(new Error('请输入0-100之间的数字'))
+          } else {
+            callback()
+          }
+        }
+      }
+      var checkName = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('大项名称不能为空'))
+        }
+        if (this.IsExistName(this.NewTitleGroup.name)) {
+          callback(new Error('大项名称已存在'))
+        } else {
+          callback()
+        }
+      }
+      return {
+        isError: false,
+        errorMsg: '',
+        currentDataSet: [],
+        modifyDisabled: false,
+        titleDisabled: [],
+        dialogFormVisible: false,
+        NewTitleGroup: {
+          name: '',
+          weight: null
+        },
+        rules: {
+          name: [
+            { validator: checkName, trigger: 'blur' }
+          ],
+          weight: [
+            { validator: checkWeight, trigger: 'blur' }
+          ]
+        },
+        formLabelWidth: '120px'
+      }
+    },
+    components: {
+      'AtPie': AtPie,
+      'VueSlideBar': VueSlideBar
+      // npm install vue-slide-bar --save
+    },
+    props: {
+      // 名称
+      groupName: {
+        type: String,
+        default: 'loading'
+      },
+      // 大项的数据集合
+      dataSet: {
+        type: Array,
+        default: () => []
+      }
+    },
+    methods: {
+      // 打开添加页
+      addLessonItem: function() {
+        this.dialogFormVisible = true
+        this.$refs['ruleForm'].resetFields()
+      },
+      // 计算权重总和
+      weightSum: function() {
+        let sum = 0
+        for (let i = 0; i < this.currentDataSet.length; i++) {
+          sum += this.currentDataSet[i].weight
+        }
+        return sum
+      },
+      // 点击修改按钮
+      openDialog() {
+        this.$confirm('此操作将修改权重信息, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$emit('notifyChanged', this.currentDataSet)
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消修改'
+          })
+        })
+      },
+      // 改变锁定状态
+      changeLock: function(id) {
+        this.$set(this.titleDisabled, id, !this.titleDisabled[id])
+      },
+      // (1)处理非法输入，主要是非数字，空值，null，数值前的0等情况
+      // (2)根据权重的改变修改状态
+      handleInput: function(val, id) {
+        let result = 0
+        if (isNaN(parseInt(val))) {
+          result = 0
+        } else if (parseInt(val) < 0) {
+          result = 0
+        } else if (parseInt(val) > 100) {
+          result = 100
+        } else {
+          result = parseInt(val)
+        }
+        const title = this.currentDataSet[id]
+        title.weight = result
+        title.value = result
+        this.$set(this.currentDataSet, id, title)
+        if (this.weightSum() > 100) {
+          this.modifyDisabled = false
+          this.isError = true
+          this.errorMsg = '权重之和超过100，请重新修改'
+        } else if (this.weightSum() < 100) {
+          this.modifyDisabled = false
+          this.isError = true
+          this.errorMsg = '权重之和不足100，请重新修改'
+        } else {
+          this.modifyDisabled = true
+          this.isError = false
+        }
+      },
+      dealCancel: function() {
+        this.dealDataSet()
+      },
+      // 判断名称是否存在
+      IsExistName: function(name) {
+        for (let i = 0; i < this.currentDataSet.length; i++) {
+          if (this.currentDataSet[i].name === name) {
+            return true
+          }
+        }
+        return false
+      },
+      // 添加大项
+      submitForm: function(formName) {
+        console.log(this.NewTitleGroup.name)
+        console.log(this.NewTitleGroup.weight)
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            this.dialogFormVisible = false
+            this.$emit('notifyAdd', this.NewTitleGroup)
+          } else {
+            return false
+          }
+        })
+      },
+      // 一键分配剩余
+      dealLeft: function() {
+        const newDataSet = this.currentDataSet
+        if (this.weightSum() < 100 && this.weightSum() >= 0) {
+          let space = 100 - this.weightSum()
+          const copySpace = space
+          while (space > 0) {
+            let i = 0
+            for (;i < newDataSet.length; i++) {
+              if (space === 0) break
+              if (this.titleDisabled[i] !== true && newDataSet[i].weight < 100) {
+                newDataSet[i].weight++
+                newDataSet[i].value++
+                space--
+              }
+            }
+            if (space === copySpace) break
+          }
+        } else if (this.weightSum() > 100) {
+          let space = this.weightSum() - 100
+          const copySpace = space
+          while (space > 0) {
+            let i = 0
+            for (;i < newDataSet.length; i++) {
+              if (space === 0) break
+              if (this.titleDisabled[i] !== true && newDataSet[i].weight > 0) {
+                newDataSet[i].weight--
+                newDataSet[i].value--
+                space--
+              }
+            }
+            if (space === copySpace) break
+          }
+        }
+        for (let i = 0; i < newDataSet.length; i++) {
+          this.$set(this.currentDataSet, i, newDataSet[i])
+        }
+      },
+      // 数据预处理
+      initLocks: function() {
+        for (let i = 0; i < this.currentDataSet.length; i++) {
+          this.titleDisabled[i] = false
+        }
+      },
+      // 处理变化的数据集
+      dealDataSet: function() {
+        if (this.currentDataSet.length > 0) this.currentDataSet = []
+        for (let i = 0; i < this.dataSet.length; i++) {
+          this.currentDataSet.push({
+            id: this.dataSet[i].id,
+            name: this.dataSet[i].name,
+            lesson_id: this.dataSet[i].lesson_id,
+            weight: this.dataSet[i].weight,
+            value: this.dataSet[i].weight
+          })
+        }
+        if (this.weightSum() === 100) {
+          this.modifyDisabled = false
+        } else {
+          this.modifyDisabled = true
+        }
+      }
+    },
+    watch: {
+      dataSet: function(val) {
+        this.dealDataSet()
+      }
+    },
+    created() {
+      this.initLocks()
+      this.dealDataSet()
+      console.log('this.dataSet' + this.dataSet)
+    },
+    updated() {
+      if (this.weightSum() > 100) {
+        this.modifyDisabled = true
+        this.isError = true
+        this.errorMsg = '权重之和超过100，请重新修改'
+      } else if (this.weightSum() < 100) {
+        this.modifyDisabled = true
+        this.isError = true
+        this.errorMsg = '权重之和不足100，请重新修改或者一键分配'
+      } else {
+        this.modifyDisabled = false
+        this.isError = false
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  $grey: #999;
+  $black: #212121;
+  $white: #FFF;
+  .column-frame{
+    display: flex;
+    flex-direction: column;
+  }
+  .row-frame1{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .row-frame2{
+    display: flex;
+    flex-direction: row;
+  }
+  .error-box{
+    width: 400px;
+    /*border:1px solid #999999;*/
+  }
+  .titles{
+    padding-bottom: 20px;
+    margin-left: 10%;
+    width: 50%;
+  }
+
+  .box-card {
+    width: 100%;
+    min-width: 800px;
+    margin: 10px 0;
+  }
+
+  .title-box{
+    display: flex;
+    align-items:baseline;
+    /*margin: 5px;*/
+    /*border: 1px solid #999;*/
+  }
+  .tag-box{
+    width: 10%;
+    min-width: 100px;
+  }
+  .slider-box{
+    width: 70%;
+    /*min-width: 300px;*/
+    margin-left: 20px;
+    /*border: 1px solid #999;*/
+  }
+  .lock-box{
+    width: 15%;
+    color: lightgrey;
+    padding: 10px;
+    &.active{
+      color: #00B2EE;
+    }
+    /*border: 1px solid #999999;*/
+  }
+  .inputBox{
+    /*width: 15%;*/
+    width: 80px;
+    margin-left: 5px;
+    /*border: 1px solid #999;*/
+  }
+  .pie-box{
+    width: 40%;
+    /*border: 1px solid #CCCCCC;*/
+  }
+  .groupName{
+    width: 100px;
+    padding: 5px;
+    font-size: larger;
+  }
+
+</style>
+
--- src/views/weight/index.vue
+++ src/views/weight/index.vue
@@ -1,30 +1,240 @@
 <!-- Tree Structure of Weight Setting -->
-<!-- 
+<!--
 div(head)
-    |- TODO: text area
-    |   |- TODO: list
-    |       |- TODO: grouptitle
-    |       |- TODO: grouptitle weight-text-fill-field
-    |       |- TODO: item-type-general
-    |       |   |- TODO: draggable scrollbar 
-    |       |- TODO: item-type-examine
-    |       |   |- TODO: title weight-text-fill-field
-    |       |- TODO: submit button
-    |- TODO: charts area
-        |- TODO: main pie(grouptitle pie)
-        |- TODO: group pies 
+    |- text area
+    |   |- list
+    |       |- grouptitle
+    |       |- grouptitle weight-text-fill-field
+    |       |- item-type-general
+    |       |   |- draggable scrollbar
+    |       |- item-type-examine
+    |       |   |-  title weight-text-fill-field
+    |       |- submit button
+    |- charts area
+        |- main pie(grouptitle pie)
+        |- group pies
 -->
 <template>
-    <div>
-        1312314
+  <div class="app-container">  <!--选择-->
+    <div class="select-box">
+      <el-select v-model="value" placeholder="请选择课程" @change="selectedCourse">
+        <el-option
+          v-for="item in selectData"
+          :key="item.id"
+          :label="item.name"
+          :value="item.id">
+        </el-option>
+      </el-select>
     </div>
+   <div v-if="empty" class="loading-box">
+   <el-button type="primary" >请选择调整的课程</el-button>
+   </div>
+    <div v-if="!empty">
+      <template v-if="titleGroups.length > 0">
+        <at-lesson-card
+          groupName="权重大项"
+          :dataSet="titleGroups"
+          @notifyChanged="handleBigChanged"
+          @notifyAdd="handleAddTitleGroup">
+        </at-lesson-card>
+      </template>
+
+      <template v-else>
+        <at-titlegroup-card class="title_box"
+          v-for="(item, index) in titleGroupsInfo"
+          :key="index"
+          :groupName="item.name"
+          :dataSet="titles[item.id]"
+          :groupId="item.id"
+          @notifyChanged="handleTitleChanged"
+          @notifyAdd="handleAddTitle">
+        </at-titlegroup-card>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-}
+  import lessonViewModel from '@/viewmodel/lesson'
+  import titleViewModel from '@/viewmodel/title'
+  import classInfoModel from '@/viewmodel/classInfo'
+  import AtTitlegroupCard from '@/components/Weight/TitlegroupCard'
+  import AtLessonCard from '@/components/Weight/LessonCard'
+  export default {
+    components: { AtTitlegroupCard, AtLessonCard },
+    data() {
+      return {
+        selectData: [], // 课程组列表/教学班列表
+        titleGroups: [], // 某一课程组的大类列表（管理员状态下）
+        titleGroupsInfo: [], // 某一课程组的大类列表（教师状态下），可能存在某一教学班的大项下无小项的情况
+        titles: {}, // 某一课程的全部小类
+        value: '', // 选择列表的值
+        empty: true // 页面显示控制
+      }
+    },
+    methods: {
+      // 下拉列表数据
+      buildSelectData: function(data) {
+        this.selectData = data
+      },
+      // 管理员/教师 大类列表(角色判断)
+      buildTitleGroup: function(titlegroups) {
+        // this.titleGroups = titlegroups // (管理员)
+        this.titleGroupsInfo = titlegroups // 教师
+      },
+      // 教师某一教学班的所有小项（按照titleGroup_id分类）
+      buildTitles: function(titles) {
+        this.titleGroups = []
+        this.titles = {}
+        for (const title of titles) {
+          if (!this.titles[title.titleGroup_id]) {
+            this.$set(this.titles, title.titleGroup_id, [title])
+          } else {
+            this.titles[title.titleGroup_id].push(title)
+          }
+        }
+        console.log(this.titles)
+      },
+      // 小项数据修改(教师)
+      handleTitleChanged: function(idx, newDataSet) {
+        titleViewModel
+          .requestPutTitles(newDataSet).then(response => {
+            this.$set(this.titles, idx, newDataSet)
+            this.$message({
+              message: '修改小项成功',
+              type: 'success'
+            })
+          })
+      },
+      // 大项数据修改（管理员）
+      handleBigChanged: function(newDataSet) {
+        titleViewModel
+          .requestPutTitleGroups(newDataSet).then(response => {
+            this.titleGroups = [...newDataSet]
+            this.$message({
+              message: '修改大项成功',
+              type: 'success'
+            })
+          })
+      },
+      // 添加一条大项记录（管理员）
+      handleAddTitleGroup: function(NewTitleGroup) {
+        console.log('NewTitle' + NewTitleGroup.name + ' ' + NewTitleGroup.weight)
+        const TitleGroup = {
+          name: NewTitleGroup.name,
+          lession_id: this.value,
+          weight: NewTitleGroup.weight
+        }
+        titleViewModel
+          .requestPostTitleGroup(TitleGroup).then(response => {
+            this.titleGroups.push(TitleGroup)
+            this.$message({
+              message: '添加大项成功',
+              type: 'success'
+            })
+          })
+      },
+      // 添加一条小项记录（教师）
+      handleAddTitle: function(NewTitle, titleGroup_id) {
+        console.log('NewTitle' + NewTitle.name + ' ' + NewTitle.weight)
+        console.log('titleGroup_id' + titleGroup_id)
+        const Title = {
+          name: NewTitle.name,
+          weight: NewTitle.weight,
+          titleGroup_id: titleGroup_id,
+          classInfo_id: this.value
+        }
+        titleViewModel
+          .requestPostTitle(Title).then(response => {
+            if (!this.titles[titleGroup_id]) { // 若该大项下没有小项
+              this.$set(this.titles, titleGroup_id, [Title])
+            } else {
+              this.titles[titleGroup_id].push(Title)
+            }
+            this.$message({
+              message: '添加小项成功',
+              type: 'success'
+            })
+          })
+      },
+      // 根据college_id获取该学校课程组信息Lesson（管理员）
+      fetchLessonData() {
+        lessonViewModel
+          .requestLesson(3)
+          .then(response => {
+            this.buildSelectData(response.subjects)
+          })
+      },
+      // 根据lesson_id获取该课程组的大项信息titleGroup（管理员）
+      fetchTitleGroup(lesson_id) {
+        titleViewModel
+          .requestTitleGroups()
+          .then(response => {
+            this.buildTitleGroup(response.subjects)
+          })
+      },
+      // 根据 teacher_id获取该教师的教学班ClassInfo（教师）
+      fetchClassInfoData() {
+        classInfoModel
+          .requestClassInfo('孔老师')
+          .then(response => {
+            this.buildSelectData(response.subjects)
+          })
+      },
+      // 根据classInfo_id 获取该课程的所有小项信息（教师）
+      fetchTitlesData() {
+        titleViewModel
+          .requestTitles(this.value)
+          .then(response => {
+            this.buildTitles(response.subjects)
+          })
+      },
+      // 列表选中值改变时  (角色判断)
+      selectedCourse: function(data_id) {
+        console.log(data_id)
+        // this.fetchTitleGroup(data_id) //当前角色是管理员
+        this.fetchTitleGroup(this.selectData[data_id].lession_id)// 当前角色是教师
+        this.fetchTitlesData() // 当前角色是教师
+        this.empty = false
+      }
+    },
+    // (角色判断)
+    created() {
+      // this.fetchLessonData()   //当前角色是管理员
+      this.fetchClassInfoData() // 当前角色是教师
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
+  $grey: #999;
+  $black: #212121;
+  $white: #FFF;
+  .select-box{
+    display: flex;
+    padding-left: 40px;
+    align-items: center;
+    background: white;
+    height: 60px;
+    border: 1px solid #CCCCCC;
+  }
+  .primary-card{
+    background-color: #d3dce6;
+    z-index: -100;
+  }
+
+  .loading-box{
+    display: flex;
+    height: 400px;
+    background: white;
+    margin-top: 20px;
+    font-size: 20px;
+    font-weight: bold;
+    font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+    color: #999999;
+    border: 1px solid #CCCCCC;
+    align-items: center;
+    justify-content: center;
+  }
 
 </style>
