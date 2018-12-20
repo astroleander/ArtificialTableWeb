4e5193989f5d379271e077e3ac6223e37b53752b
liqian <2535460824@qq.com>
2018-12-20 21:03:29 +0800

    api文件--login.js修改

--- src/api/login.js
+++ src/api/login.js
@@ -3,7 +3,6 @@ import urls from '@/urls'
 
 export function login(username, password) {
   return request({
-    // url: urls.login.uri,
     url: urls.login,
     method: 'post',
     data: {
@@ -14,38 +13,11 @@ export function login(username, password) {
 }
 
 /**
- *
- * @param token
- * @param id
- */
-export function getInfo({ token, id }) {
-  return request({
-    url: '/user/info/format',
-    // url: urls.logout,
-    method: 'get',
-    params: { token, id }
-  })
-}
-
-/**
  * 退出登录
  */
 export function logout() {
   return request({
     url: '/user/logout',
     method: 'post'
-  })
-}
-
-/**
- * 注册
- */
-export function regist(User) {
-  return request({
-    url: '/user/logon',
-    method: 'post',
-    data: {
-      subjects: [User]
-    }
   })
 }
