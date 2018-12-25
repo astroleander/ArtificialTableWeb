e347528545743ba585c12e51bf31bbdec0387a88
liqian <2535460824@qq.com>
2018-12-25 16:44:38 +0800

    1.修改了utils文件的auth.js   添加了在cookie中user的get、set和remove方法 2.修改了store/modules文件中的user.js   主要修改了action中的方法，使用api的部分全部抽到viewmodel层，这里action中 的方法仅用来存储数据。 3.修改viewmodel层的login.js   使用api层的请求后，将登录成功后请求到的数据存储到vuex中

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,13 +1,10 @@
-import { login, logout, getInfo } from '@/api/login'
-import { getToken, setToken, removeToken, getId, setId, removeId } from '@/utils/auth'
+import { getToken, setToken, removeToken, getId, setId, removeId, getUser, setUser, removeUser } from '@/utils/auth'
 
 const user = {
   state: {
     token: getToken(),
     id: getId(),
-
-    name: '',
-    manager: false
+    user: getUser()
   },
 
   mutations: {
@@ -17,9 +14,8 @@ const user = {
     SET_ID: (state, id) => {
       state.id = id
     },
-
-    SET_NAME: (state, name) => {
-      state.name = name
+    SET_USER: (state, user) => {
+      state.user = user
     },
     SET_GROUPS: (state, group) => {
       state.group = group
@@ -28,60 +24,22 @@ const user = {
 
   actions: {
     // 登录
-    Login({ commit }, userInfo) {
-      const username = userInfo.username.trim()
-      return new Promise((resolve, reject) => {
-        login(username, userInfo.password).then(response => {
-          const data = response
-          try {
-            setToken(data.subjects.token)
-            setId(data.subjects.id)
-
-            commit('SET_TOKEN', data.subjects.token)
-            commit('SET_ID', data.subjects.id)
-          } catch (err) {
-            const message = '\ncode:' + data.code + '\nmessage:' + data.message
-            throw new Error(message)
-          }
-          resolve()
-        }).catch(error => {
-          reject(error)
-        })
-      })
+    Login({ commit }, data) {
+      setToken(data.token)
+      setId(data.id)
+      setUser(data.user)
+      commit('SET_TOKEN', data.token)
+      commit('SET_ID', data.id)
+      commit('SET_USER', data.user)
     },
-
-    // 获取用户信息
-    GetInfo({ commit, state }) {
-      return new Promise((resolve, reject) => {
-        const token = state.token
-        const id = state.id
-        getInfo({ token, id }).then(response => {
-          // set user info
-          // commit('SET_GROUPS', data.groups)
-          const data = response && response.subjects && response.subjects[0]
-          // TODO: refactor user profile info in vuex
-          commit('SET_NAME', data.name)
-          commit('SET_MANAGER', data.isManager)
-          resolve(response)
-        }).catch(error => {
-          reject(error)
-        })
-      })
-    },
-
     // 登出
     LogOut({ commit, state }) {
-      return new Promise((resolve, reject) => {
-        logout(state.token).then(() => {
-          commit('SET_TOKEN', '')
-          commit('SET_ID', '')
-          removeToken()
-          removeId()
-          resolve()
-        }).catch(error => {
-          reject(error)
-        })
-      })
+      commit('SET_TOKEN', '')
+      commit('SET_ID', '')
+      commit('SET_USER', null)
+      removeToken()
+      removeId()
+      removeUser()
     },
 
     // 仅前端 登出
--- src/viewmodel/login.js
+++ src/viewmodel/login.js
@@ -1,25 +1,25 @@
 import store from '@/store'
-import { login } from '@/api/login'
-// TODO: change data -> viewmodel
-// const login = (form) => {
-//   return new Promise((resolve, reject) => {
-//     store.dispatch('Login', form)
-//       .then(() => resolve())
-//       .catch(err => {
-//         reject(err)
-//       })
-//   })
-// }
+import { login, logout } from '@/api/login'
+import { getUsersWithOutPass } from '@/api/user'
 
-const requestlogin = (form) => {
-  const username = form.username.trim()
-  const password = form.password
+const requestLogin = ({ username, password }) => {
   return new Promise((resolve, reject) => {
     login(username, password).then(response => {
-      const data = response
+      const data = response // response里有id和token
       if (data.subjects !== undefined) {
-        store.commit('SET_TOKEN', data.subjects.token)
-        store.commit('SET_ID', data.subjects.id)
+        // 获取用户信息
+        getUsersWithOutPass({ id: data.subjects.id })
+          .then(response => {
+            if (response.subjects !== undefined) {
+              store.dispatch('Login', {
+                id: data.subjects.id,
+                token: data.subjects.token,
+                user: response.subjects[0]
+              })
+            }
+          }).catch(error => {
+            reject(error)
+          })
       } else {
         const message = '\ncode:' + data.code + '\nmessage:' + data.message
         throw new Error(message)
@@ -30,6 +30,16 @@ const requestlogin = (form) => {
     })
   })
 }
+const requestLogOut = () => {
+  return new Promise((resolve, reject) => {
+    logout().then(() => {
+      store.dispatch('LogOut')
+      resolve()
+    }).catch(error => {
+      reject(error)
+    })
+  })
+}
 export default{
-  requestlogin
+  requestLogin, requestLogOut
 }
