0d28d0ab6ae08198f2fcb1f8b97a2d935f82ffee
B2015212110 <1651290238@qq.com>
2019-06-11 16:18:21 +0800

    修复版本配置问题

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  //BASE_API: '"http://119.29.63.216:8000/api/v1"'
+  // BASE_API: '"http://119.29.63.216:8000/api/v1"'
   BASE_API: '"http://123.207.164.113:8000/api/v1"'
+   // BASE_API: '"http://localhost:8000/api/v1"'
 });
